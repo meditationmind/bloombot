757be4907f64ff426ceaa46f45de456bf6d2f744
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2024-11-02 00:17:58 +0900

    Improve flow control

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -18,8 +18,7 @@ pub async fn quote(
   #[description = "Refine quote pool with one or more keywords in search engine format"]
   keyword: Option<String>,
 ) -> Result<()> {
-  let data = ctx.data();
-  let mut transaction = data.db.start_transaction_with_retry(5).await?;
+  let mut transaction = ctx.data().db.start_transaction_with_retry(5).await?;
 
   let guild_id = ctx
     .guild_id()
@@ -27,57 +26,37 @@ pub async fn quote(
 
   if let Some(keyword) = keyword {
     if common::is_supporter(ctx).await? {
-      match DatabaseHandler::get_random_quote_with_keyword(&mut transaction, &guild_id, &keyword)
-        .await?
+      if let Some(quote) =
+        DatabaseHandler::get_random_quote_with_keyword(&mut transaction, &guild_id, &keyword)
+          .await?
       {
-        None => {
-          ctx
-            .send(
-              poise::CreateReply::default()
-                .content("No quotes found. Fetching random quote.")
-                .ephemeral(true),
-            )
-            .await?;
+        let embed = BloomBotEmbed::new().description(format!(
+          "{}\n\n\\― {}",
+          quote.quote,
+          quote.author.unwrap_or("Anonymous".to_string())
+        ));
 
-          match DatabaseHandler::get_random_quote(&mut transaction, &guild_id).await? {
-            None => {
-              ctx.say("No quotes found.").await?;
-            }
-            Some(quote) => {
-              let embed = BloomBotEmbed::new()
-                .description(format!(
-                  "{}\n\n\\― {}",
-                  quote.quote.as_str(),
-                  quote.author.unwrap_or("Anonymous".to_string())
-                ))
-                .clone();
+        ctx.send(poise::CreateReply::default().embed(embed)).await?;
 
-              ctx.send(poise::CreateReply::default().embed(embed)).await?;
-            }
-          }
-
-          return Ok(());
-        }
-        Some(quote) => {
-          let embed = BloomBotEmbed::new()
-            .description(format!(
-              "{}\n\n\\― {}",
-              quote.quote.as_str(),
-              quote.author.unwrap_or("Anonymous".to_string())
-            ))
-            .clone();
-
-          ctx.send(poise::CreateReply::default().embed(embed)).await?;
-        }
+        return Ok(());
       }
-
-      return Ok(());
+      ctx
+        .send(
+          poise::CreateReply::default()
+            .content("No quotes found. Fetching random quote.")
+            .ephemeral(true),
+        )
+        .await?;
     }
 
     ctx
       .send(
         poise::CreateReply::default()
-          .content(format!("{} The keyword option is only available to [subscription-based donators](<https://discord.com/channels/244917432383176705/1030424719138246667/1031137243345211413>).", EMOJI.mminfo))
+          .content(format!(
+            "{} The keyword option is only available to [subscription-based donators]\
+            (<https://discord.com/channels/244917432383176705/1030424719138246667/1031137243345211413>).",
+            EMOJI.mminfo
+          ))
           .ephemeral(true),
       )
       .await?;
@@ -88,13 +67,11 @@ pub async fn quote(
       ctx.say("No quotes found.").await?;
     }
     Some(quote) => {
-      let embed = BloomBotEmbed::new()
-        .description(format!(
-          "{}\n\n\\― {}",
-          quote.quote.as_str(),
-          quote.author.unwrap_or("Anonymous".to_string())
-        ))
-        .clone();
+      let embed = BloomBotEmbed::new().description(format!(
+        "{}\n\n\\― {}",
+        quote.quote,
+        quote.author.unwrap_or("Anonymous".to_string())
+      ));
 
       ctx.send(poise::CreateReply::default().embed(embed)).await?;
     }
