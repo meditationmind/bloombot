b7a834089ba5e3ff0c5c031ded47feaa090b84b1
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2024-11-16 06:44:16 +0900

    Fix arguments
    Fixed reversed order of arguments for Ko-fi event; reorganized flow control to check new member welcomes first

--- src/events/guild_member_update.rs
+++ src/events/guild_member_update.rs
@@ -14,12 +14,12 @@ impl UpdateType {
     let patreon_role = RoleId::new(ROLES.patreon);
     let kofi_role = RoleId::new(ROLES.kofi);
 
-    if !old.roles.contains(&patreon_role) && new.roles.contains(&patreon_role) {
+    if old.pending && !new.pending {
+      Some(Self::StoppedPending)
+    } else if !old.roles.contains(&patreon_role) && new.roles.contains(&patreon_role) {
       Some(Self::BecamePatreonDonator)
     } else if !old.roles.contains(&kofi_role) && new.roles.contains(&kofi_role) {
       Some(Self::BecameKofiDonator)
-    } else if old.pending && !new.pending {
-      Some(Self::StoppedPending)
     } else {
       None
     }
@@ -28,65 +28,61 @@ impl UpdateType {
 
 pub async fn guild_member_update(
   ctx: &Context,
-  old_if_available: &Option<Member>,
+  old: &Option<Member>,
   new: &Option<Member>,
 ) -> Result<()> {
-  let Some(old) = old_if_available else {
+  let Some(old) = old else { return Ok(()) };
+  let Some(new) = new else { return Ok(()) };
+  let Some(update_type) = UpdateType::get_type(old, new) else {
     return Ok(());
   };
-  let Some(new) = new else { return Ok(()) };
 
-  if let Some(update_type) = UpdateType::get_type(old, new) {
-    match update_type {
-      UpdateType::BecamePatreonDonator => {
-        let donator_channel = ChannelId::new(CHANNELS.donators);
+  match update_type {
+    UpdateType::StoppedPending => {
+      let welcome_channel = ChannelId::new(CHANNELS.welcome);
+      let msg = format!(
+        "Please give <@{}> a warm welcome, <@&{}>!",
+        new.user.id, ROLES.welcome_team
+      );
+      let embed = BloomBotEmbed::new()
+        .title(":tada: A new member has arrived! :tada:")
+        .description(format!(
+          "Welcome to the Meditation Mind community, <@{}>!\n\nCheck out <id:customize> to grab some roles and [customize your community experience](<https://meditationmind.org/curating-your-experience/>).\n\nWe're glad you've joined us! {}",
+          new.user.id, EMOJI.aww
+        ))
+        .thumbnail(
+          "https://meditationmind.org/wp-content/uploads/2020/04/Webp.net-resizeimage-1.png",
+        );
 
-        donator_channel
-          .send_message(&ctx, CreateMessage::new()
-            .embed(BloomBotEmbed::new()
-              .title(":tada: New Donator :tada:")
-              .description(format!(
-                "Please welcome <@{}> as a new donator on Patreon.\n\nThank you for your generosity! It helps keep this community alive {}",
-                new.user.id,
-                EMOJI.loveit
-              ))
-            )
-          )
-          .await?;
-      }
-      UpdateType::BecameKofiDonator => {
-        let donator_channel = ChannelId::new(CHANNELS.donators);
+      welcome_channel
+        .send_message(&ctx, CreateMessage::new().content(msg).embed(embed))
+        .await?;
+    }
+    UpdateType::BecamePatreonDonator => {
+      let donator_channel = ChannelId::new(CHANNELS.donators);
+      let embed = BloomBotEmbed::new()
+        .title(":tada: New Donator :tada:")
+        .description(format!(
+          "Please welcome <@{}> as a new donator on Patreon.\n\nThank you for your generosity! It helps keep this community alive {}",
+          new.user.id, EMOJI.loveit
+        ));
 
-        donator_channel
-          .send_message(&ctx, CreateMessage::new()
-            .embed(BloomBotEmbed::new()
-              .title(":tada: New Donator :tada:")
-              .description(format!(
-                "Please welcome <@{}> as a new donator on Ko-fi.\n\nThank you for your generosity! It helps keep this community alive {}",
-                EMOJI.loveit,
-                new.user.id
-              ))
-            )
-          )
-          .await?;
-      }
-      UpdateType::StoppedPending => {
-        let welcome_channel = ChannelId::new(CHANNELS.welcome);
+      donator_channel
+        .send_message(&ctx, CreateMessage::new().embed(embed))
+        .await?;
+    }
+    UpdateType::BecameKofiDonator => {
+      let donator_channel = ChannelId::new(CHANNELS.donators);
+      let embed = BloomBotEmbed::new()
+        .title(":tada: New Donator :tada:")
+        .description(format!(
+          "Please welcome <@{}> as a new donator on Ko-fi.\n\nThank you for your generosity! It helps keep this community alive {}",
+          new.user.id, EMOJI.loveit,
+        ));
 
-        welcome_channel
-          .send_message(&ctx, CreateMessage::new()
-            .content(format!("Please give <@{}> a warm welcome, <@&{}>!", new.user.id, ROLES.welcome_team))
-              .embed(BloomBotEmbed::new()
-                  .title(":tada: A new member has arrived! :tada:")
-                  .description(format!(
-                    "Welcome to the Meditation Mind community, <@{}>!\n\nCheck out <id:customize> to grab some roles and [customize your community experience](<https://meditationmind.org/curating-your-experience/>).\n\nWe're glad you've joined us! {}",
-                    new.user.id, EMOJI.aww
-                  ))
-                  .thumbnail("https://meditationmind.org/wp-content/uploads/2020/04/Webp.net-resizeimage-1.png")
-            )
-          )
-          .await?;
-      }
+      donator_channel
+        .send_message(&ctx, CreateMessage::new().embed(embed))
+        .await?;
     }
   }
 
