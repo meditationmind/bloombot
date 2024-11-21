4f194a6a204209dc1efc99abe3312d3bbce0a50d
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2024-11-22 05:14:51 +0900

    Update error handler
    Small tweaks; make command error message ephemeral; get rid of debug display for error

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::sync::Arc;
 use std::time::Instant;
 
 use anyhow::{anyhow, Context as ErrorContext, Error, Result};
+use config::EMOJI;
 use dotenvy::dotenv;
 use log::{error, info};
 use poise::serenity_prelude::{ActivityData, Channel, Client, GatewayIntents, GuildId};
@@ -148,11 +149,16 @@ async fn main() -> Result<()> {
 async fn error_handler(error: FrameworkError<'_, Data, Error>) {
   match error {
     FrameworkError::Command { ctx, error, .. } => {
-      match ctx.say("An error occurred while running the command").await {
-        Ok(_) => {}
-        Err(e) => {
-          error!("While handling error, could not send message: {e}");
-        }
+      let msg = format!(
+        "{} An error occurred while running the command. \
+        Please try again or contact server staff for assistance.",
+        EMOJI.mminfo
+      );
+      if let Err(e) = ctx
+        .send(CreateReply::default().content(msg).ephemeral(true))
+        .await
+      {
+        error!("While handling error, could not send message: {e}");
       };
 
       let command = ctx.command();
@@ -160,7 +166,7 @@ async fn error_handler(error: FrameworkError<'_, Data, Error>) {
       let channel = if let Ok(channel) = channel_id.to_channel(ctx).await {
         Some(channel)
       } else {
-        error!("While handling error, could not get channel {channel_id}");
+        error!("While handling error, could not get channel: {channel_id}");
         None
       };
 
@@ -168,30 +174,23 @@ async fn error_handler(error: FrameworkError<'_, Data, Error>) {
       let source = match &channel {
         Some(channel) => match channel {
           Channel::Guild(_) => {
-            let guild_name = match ctx.guild() {
-              Some(guild) => guild.name.clone(),
-              None => "unknown".to_owned(),
-            };
-            format!("{} ({})", guild_name, channel.id())
+            let guild_name = ctx
+              .guild()
+              .map_or("unknown".to_owned(), |guild| guild.name.clone());
+            format!("{guild_name} ({})", channel.id())
           }
           Channel::Private(_) => "DM".to_owned(),
           _ => "unknown".to_owned(),
         },
         None => "unknown".to_owned(),
       };
-      let user = ctx.author();
 
-      error!(
-        "\x1B[1m/{}\x1B[0m failed with error: {:?}",
-        command.name, error
-      );
+      error!("\x1B[1m/{}\x1B[0m failed with error: {error}", command.name);
       error!("\tSource: {source}");
-
       if let Some(channel) = channel {
         error!("\tChannel: {}", channel.id());
       }
-
-      error!("\tUser: {} ({})", user.name, user.id);
+      error!("\tUser: {} ({})", ctx.author().name, ctx.author().id);
     }
     FrameworkError::ArgumentParse {
       error, input, ctx, ..
@@ -201,16 +200,12 @@ async fn error_handler(error: FrameworkError<'_, Data, Error>) {
       } else {
         format!("**{error}**")
       };
-
-      match ctx
+      if let Err(e) = ctx
         .send(CreateReply::default().content(response).ephemeral(true))
         .await
       {
-        Ok(_) => {}
-        Err(e) => {
-          error!("While handling error, could not send message: {e}");
-        }
-      };
+        error!("While handling error, could not send message: {e}");
+      }
     }
     error => {
       if let Err(e) = builtins::on_error(error).await {
