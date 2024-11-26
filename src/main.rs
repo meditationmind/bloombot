9ae932c7a57b1e05f358568fb73afddbf59ab354
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2024-11-26 19:57:55 +0900

    Update error handler
    Change command name to qualified name to include parent command; handle unknown member errors

--- src/main.rs
+++ src/main.rs
@@ -180,19 +180,32 @@ async fn main() -> Result<()> {
 async fn error_handler(error: FrameworkError<'_, Data, Error>) {
   match error {
     FrameworkError::Command { ctx, error, .. } => {
-      let msg = format!(
-        "{} An error occurred while running the command. \
-        Please try again or contact server staff for assistance.",
-        EMOJI.mminfo
-      );
-      if let Err(e) = ctx
-        .send(CreateReply::default().content(msg).ephemeral(true))
-        .await
-      {
-        error!("While handling error, could not send message: {e}");
-      };
+      if error.to_string() == "Unknown Member" {
+        let msg = format!(
+          "{} The specified user is not a member of this server.",
+          EMOJI.mminfo
+        );
+        match ctx
+          .send(CreateReply::default().content(msg).ephemeral(true))
+          .await
+        {
+          Ok(_) => return,
+          Err(e) => error!("While handling error, could not send message: {e}"),
+        }
+      } else {
+        let msg = format!(
+          "{} An error occurred. Please try again or contact server staff for assistance.",
+          EMOJI.mminfo
+        );
+        if let Err(e) = ctx
+          .send(CreateReply::default().content(msg).ephemeral(true))
+          .await
+        {
+          error!("While handling error, could not send message: {e}");
+        };
+      }
 
-      let command = ctx.command();
+      let command_name = ctx.command().qualified_name.as_str();
       let channel_id = ctx.channel_id();
       let channel = if let Ok(channel) = channel_id.to_channel(ctx).await {
         Some(channel)
@@ -201,9 +214,10 @@ async fn error_handler(error: FrameworkError<'_, Data, Error>) {
         None
       };
 
-      // Whether it's a guild or DM channel
-      let source = match &channel {
-        Some(channel) => match channel {
+      // Check whether the error originated from a guild channel or DM.
+      let source = channel
+        .as_ref()
+        .map_or("unknown".to_owned(), |channel| match channel {
           Channel::Guild(_) => {
             let guild_name = ctx
               .guild()
@@ -212,11 +226,9 @@ async fn error_handler(error: FrameworkError<'_, Data, Error>) {
           }
           Channel::Private(_) => "DM".to_owned(),
           _ => "unknown".to_owned(),
-        },
-        None => "unknown".to_owned(),
-      };
+        });
 
-      error!("\x1B[1m/{}\x1B[0m failed with error: {error}", command.name);
+      error!("\x1B[1m/{command_name}\x1B[0m failed with error: {error}");
       error!("\tSource: {source}");
       if let Some(channel) = channel {
         error!("\tChannel: {}", channel.id());
