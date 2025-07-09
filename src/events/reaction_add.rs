3ecd877d6a68afc8d4a4a825cd40c4504cd458c3
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2025-07-09 16:23:57 +0900

    Improve reaction report error handling

--- src/events/reaction_add.rs
+++ src/events/reaction_add.rs
@@ -1,6 +1,7 @@
 use anyhow::{Context as AnyhowContext, Result};
 use poise::serenity_prelude::{ChannelId, Context, CreateEmbedAuthor, CreateEmbedFooter};
 use poise::serenity_prelude::{CreateMessage, Reaction, ReactionType};
+use tracing::{error, info};
 
 use crate::config::{BloomBotEmbed, CHANNELS, EMOTES, ROLES};
 use crate::database::DatabaseHandler;
@@ -28,59 +29,68 @@ pub async fn reaction_add(
 }
 
 async fn check_report(ctx: &Context, reaction: &Reaction) -> Result<()> {
-  if let ReactionType::Custom { id, .. } = reaction.emoji {
-    if id == EMOTES.report {
-      // Remove reaction from message
-      reaction
-        .delete(&ctx)
-        .await
-        .with_context(|| "Failed to remove report reaction from message")?;
+  if let ReactionType::Custom { id, .. } = reaction.emoji
+    && id == EMOTES.report
+  {
+    // Remove reaction from message.
+    let reaction_removed = reaction.delete(&ctx).await.is_ok();
 
-      let report_channel_id = ChannelId::from(CHANNELS.reportchannel);
-      let message = reaction.message(&ctx).await?;
-      let message_link = message.link();
-      let message_user = message.author;
-      let message_channel_name = message.channel_id.name(ctx).await?;
-      let reporting_user = reaction.user(&ctx).await?;
+    let report_channel_id = ChannelId::from(CHANNELS.reportchannel);
+    let message = reaction.message(&ctx).await?;
+    let message_link = message.link();
+    let message_user = message.author;
+    let message_channel_name = message.channel_id.name(ctx).await?;
+    let reporting_user = reaction.user(&ctx).await?;
 
-      let message_content = if message.content.is_empty() {
-        match message.attachments.first() {
-          Some(attachment) => format!("**Attachment**\n{}", attachment.url),
-          None => message.content,
-        }
+    let message_content = if message.content.is_empty() {
+      match message.attachments.first() {
+        Some(attachment) => format!("**Attachment**\n{}", attachment.url),
+        None => message.content,
+      }
+    } else {
+      message.content
+    };
+
+    let content = format!(
+      "{} Message Reported{}",
+      ROLES.staff,
+      if reaction_removed {
+        ""
       } else {
-        message.content
-      };
+        "\n-# *Failed to remove report emoji. Please manually remove.*"
+      }
+    );
 
-      report_channel_id
-        .send_message(
-          &ctx,
-          CreateMessage::new()
-            .content(format!("{} Message Reported", ROLES.staff))
-            .embed(
+    let embed = BloomBotEmbed::new()
+      .author(CreateEmbedAuthor::new(&message_user.name).icon_url(message_user.face()))
+      .description(message_content)
+      .field("Link", format!("[Go to message]({message_link})"), false)
+      .footer(CreateEmbedFooter::new(format!(
+        "Author ID: {}\nReported via reaction in #{} by {} ({})",
+        &message_user.id, message_channel_name, reporting_user.name, reporting_user.id
+      )))
+      .timestamp(message.timestamp);
+
+    match report_channel_id
+      .send_message(&ctx, CreateMessage::new().content(content).embed(embed))
+      .await
+    {
+      Ok(_) => {
+        if let Err(e) = reporting_user
+          .dm(
+            &ctx,
+            CreateMessage::new().embed(
               BloomBotEmbed::new()
-                .author(CreateEmbedAuthor::new(&message_user.name).icon_url(message_user.face()))
-                .description(message_content)
-                .field("Link", format!("[Go to message]({message_link})"), false)
-                .footer(CreateEmbedFooter::new(format!(
-                  "Author ID: {}\nReported via reaction in #{} by {} ({})",
-                  &message_user.id, message_channel_name, reporting_user.name, reporting_user.id
-                )))
-                .timestamp(message.timestamp),
+                .title("Report")
+                .description("Your report has been sent to the moderation team."),
             ),
-        )
-        .await?;
-
-      reporting_user
-        .dm(
-          &ctx,
-          CreateMessage::new().embed(
-            BloomBotEmbed::new()
-              .title("Report")
-              .description("Your report has been sent to the moderation team."),
-          ),
-        )
-        .await?;
+          )
+          .await
+        {
+          info!("Failed to notify user of successful reaction report: {e}");
+        }
+      }
+      Err(e) => error!("Failed to notify staff of reaction report: {e}"),
     }
   }
 
